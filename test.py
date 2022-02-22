d16c771d4a66ad84a1ec59c3ee6406f4f6547d0c
tscrivanich27 <tscrivanich27@gmail.com>
2022-02-22 11:11:36 -0500

    Implemented Unit Test #2

--- test.py
+++ test.py
@@ -23,7 +23,23 @@ def test_no_sender_ID():
     except (AssertionError):
         logging.error("Unit Test #1 Failed")
 
+# Unit Test #2: Sender ID has invalid type
+def test_wrong_sender_ID_type():
+    logging.info("Running Unit Test #2")
+    try:
+        # Get a response from the server
+        response = requests.get(BASE + "chat/wrong_sender_ID_type.json")
+         # Format the reponse as a json structure
+        arg = response.json()
+        # Check the contents of the json structure
+        assert arg == {'message':'The sender ID is not present or has an invalid type'}
+        logging.info("Unit Test #2 Succeeded")
+    # Check the contents of the json structure
+    except (AssertionError):
+        logging.error("Unit Test #2 Failed")
+
 # Main Function
 if __name__ == "__main__":
     test_no_sender_ID()
+    test_wrong_sender_ID_type()
     logging.info("Ran All Tests")
